801f2d4975ce12b10b4f5821c3bc0f3985cf7540
Oxel40 <Oxel40@users.noreply.github.com>
2022-12-06 11:10:46 +0100

    kinda ok

--- hmm3.py
+++ hmm3.py
@@ -1,4 +1,5 @@
 from la import *
+import sys
 
 
 # Init variables
@@ -9,7 +10,7 @@ seq = list(map(int, input().split()[1:]))
 
 n_states = init.cols
 
-a_mat = Matrix(len(seq), n_states)
+alpha_mat = Matrix(len(seq), n_states)
 beta_mat = Matrix(len(seq),n_states)
 
 di_gamma = [Matrix(n_states,n_states) for _ in range(len(seq))]
@@ -21,38 +22,38 @@ scale = [0]*len(seq)
 def alpha_pass():
     # Forward pass
     for i in range(n_states):
-        a_mat[0][i] = init[0][i] * emis[i][seq[0]]
-    scale[0] = 1/sum(a_mat[0])
+        alpha_mat[0][i] = init[0][i] * emis[i][seq[0]]
+    scale[0] = 1/sum(alpha_mat[0])
     for i in range(n_states):
-        a_mat[0][i] *= scale[0]
+        alpha_mat[0][i] *= scale[0]
 
     for t in range(1, len(seq)):
         for i in range(n_states):
-            t_prob = sum([ a_mat[t-1][j]*tran[j][i] for j in range(n_states)])
-            a_mat[t][i] = t_prob * emis[i][seq[t]]
-        scale[t] = 1/sum(a_mat[t])
+            t_prob = sum([ alpha_mat[t-1][j]*tran[j][i] for j in range(n_states)])
+            alpha_mat[t][i] = t_prob * emis[i][seq[t]]
+        scale[t] = 1/sum(alpha_mat[t])
         for i in range(n_states):
-            a_mat[t][i] *= scale[t]
+            alpha_mat[t][i] *= scale[t]
 
 
 def beta_pass():
     # Backwards pass
     for i in range(n_states):
-        beta_mat[-1][i] = 1 # * scale[-1]
+        beta_mat[-1][i] = 1 * scale[-1]
 
     for t in reversed(range(len(seq)-1)):
         for i in range(n_states):
-            beta_mat[t][i] = sum([beta_mat[t + 1][j] * emis[j][seq[t+1]] * tran[i][j] for j in range(n_states)])
+            beta_mat[t][i] = sum([beta_mat[t+1][j] * emis[j][seq[t+1]] * tran[i][j] for j in range(n_states)])
             beta_mat[t][i] *= scale[t]
 
 
 def calc_gamma():
-    sum_alpha = sum(a_mat[-1])
+    sum_alpha = sum(alpha_mat[-1])  # Might be wrong
 
     for t in range(len(seq)-1):
         for i in range(n_states):
             for j in range(n_states):
-                alpha = a_mat[t][i]
+                alpha = alpha_mat[t][i]
                 a = tran[i][j]
                 b = emis[j][seq[t+1]]
                 beta = beta_mat[t+1][j]
@@ -61,25 +62,31 @@ def calc_gamma():
 
             gamma[t][i] = sum(di_gamma[t][i])
 
+    # Special case?
+
 def reestimate():
     # Reestimate transmission matrix
     # 2.35 typ
     for i in range(n_states):
-        sum_gamma = sum([gamma[t][i] for t in range(len(seq))] )
         for j in range(n_states):
             sum_di_gamma = sum([di_gamma[t][i][j] for t in range(len(seq))])
+            sum_gamma = sum([gamma[t][i] for t in range(len(seq))] )
             tran[i][j] = sum_di_gamma / sum_gamma
 
     # Reestimate emission matrix
     # b estimates. 2.36
     for j in range(n_states):
-        for k in seq:
+        for k in range(emis.cols):
+
             sum_gamma_denominator = 0
             sum_gamma_numerator = 0
+
             for t in range(len(seq)):
                 indicator = lambda _k: 1 if _k == seq[t] else 0
+
                 sum_gamma_numerator += gamma[t][j]*indicator(k)
                 sum_gamma_denominator += gamma[t][j]
+
             emis[j][k] = sum_gamma_numerator / sum_gamma_denominator
 
     # Reestimate initial probability matrix
@@ -88,24 +95,34 @@ def reestimate():
         init[0][i] = gamma[0][i]
 
 
-prev_a_mat = -math.inf
-max_iter = 1000
+# prev_a_mat = -math.inf
+# tran_sum = sum(map(sum, tran.data))
+# emis_sum = sum(map(sum, emis.data))
+sum_scale = math.inf
+max_iter = 200
 iter = 0
 #for _ in range(2):
-while iter < max_iter:
+#while iter < max_iter:
+while True:
+    print("\r" + str(iter) + ": " + str(sum(scale)), end="", file=sys.stderr)
     iter+=1
 
     alpha_pass()
-
-    new_a_mat = sum(a_mat[-1])
-
-    if abs(new_a_mat-prev_a_mat) < 1e-20:
-        break
-    prev_a_mat = new_a_mat
-
     beta_pass()
     calc_gamma()
     reestimate()
+
+    new_sum_scale = sum(map(math.log, scale))
+    # new_tran_sum = sum(map(sum, tran.data))
+    # new_emis_sum = sum(map(sum, emis.data))
+    # if abs(new_tran_sum - tran_sum) + abs(new_emis_sum - emis_sum) > 1e-8:
+    if new_sum_scale >= sum_scale:
+        break
+    # tran_sum = new_tran_sum
+    # emis_sum = new_emis_sum
+    sum_scale = new_sum_scale
+
+print("", file=sys.stderr)
 
 
 output_matrix(tran)
